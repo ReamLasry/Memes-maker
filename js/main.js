b582479a9e350c298b6402a7e2d677a2941b8cb4
ReamLasry <ream794613@gmail.com>
2021-02-10 14:39:46 +0200

    Canvas update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,52 @@
 'use strict';
 console.log('main check');
 
+var gImages;
+var gElCanvas;
+var gCtx;
+
+
 function init() {
-    onRenderMemes();
+    gImages = imagesForDisplay();
+    console.log(gImages);
+    onRenderGallery();
+    renderCanvas();
 }
 
-function onRenderMemes() {
-    let images = imagesForDisplay();
-    let strHTML = images.map(image => {
+
+
+function renderCanvas() {
+    gElCanvas = document.querySelector('.canvas-content');
+    console.log(gElCanvas);
+    gCtx = gElCanvas.getContext('2d');
+    // onDrawImg();
+
+    console.log(meme.lines[0].txt);
+}
+
+function onDrawImg() {
+    const img = new Image();
+    img.src = gImages[0].url;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height) //img,x,y,xend,yend
+        var meme = createMeme();
+        onDrawText(meme.lines[0].txt);
+    }
+}
+
+function onDrawText(text) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'red'
+    gCtx.fillStyle = 'white'
+    gCtx.font = '40px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.fillText(text, gElCanvas.width / 2, 20)
+    gCtx.strokeText(text, gElCanvas.width / 2, 20)
+}
+
+function onRenderGallery() {
+    let strHTML = gImages.map(image => {
         return `<div class="img${image.id}"><img src="image/${image.id}.jpg" alt=""></div>`;
     }).join('');
-
     document.querySelector('.image-content').innerHTML = strHTML;
 }
---
+++ js/services/memes-service.js
@@ -0,0 +1,102 @@
+'use strict';
+console.log('memes service');
+var ggalleryImgs;
+var gMeme;
+
+
+function createMeme() {
+    gMeme = {
+        selectedImgId: 5,
+        selectedLineIdx: 0,
+        lines: [{
+            txt: 'I have gases',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }]
+    };
+    return gMeme;
+}
+
+
+function imagesForDisplay() {
+    let images = _createImages();
+    let imgsForDisplay = images.map(image => image);
+    return imgsForDisplay;
+}
+
+function _createImages() {
+    return ggalleryImgs = [{
+        id: 1,
+        url: 'image/1.jpg',
+        keywords: ['funny', 'man'],
+    }, {
+        id: 2,
+        url: 'image/2.jpg',
+        keywords: ['cute', 'animal'],
+    }, {
+        id: 3,
+        url: 'image/3.jpg',
+        keywords: ['kids', 'animal', 'cute'],
+    }, {
+        id: 4,
+        url: 'image/4.jpg',
+        keywords: ['animal', 'cute'],
+    }, {
+        id: 5,
+        url: 'image/5.jpg',
+        keywords: ['kid', 'cute'],
+    }, {
+        id: 6,
+        url: 'image/6.jpg',
+        keywords: ['man', 'funny'],
+    }, {
+        id: 7,
+        url: 'image/7.jpg',
+        keywords: ['kid', 'funny'],
+    }, {
+        id: 8,
+        url: 'image/8.jpg',
+        keywords: ['man'],
+    }, {
+        id: 9,
+        url: 'image/9.jpg',
+        keywords: ['kid', 'funny'],
+    }, {
+        id: 10,
+        url: 'image/10.jpg',
+        keywords: ['man', 'funny'],
+    }, {
+        id: 11,
+        url: 'image/11.jpg',
+        keywords: ['man'],
+    }, {
+        id: 12,
+        url: 'image/12.jpg',
+        keywords: ['man'],
+    }, {
+        id: 13,
+        url: 'image/13.jpg',
+        keywords: ['man'],
+    }, {
+        id: 14,
+        url: 'image/14.jpg',
+        keywords: ['man'],
+    }, {
+        id: 15,
+        url: 'image/15.jpg',
+        keywords: ['man'],
+    }, {
+        id: 16,
+        url: 'image/16.jpg',
+        keywords: ['man', 'funny'],
+    }, {
+        id: 17,
+        url: 'image/17.jpg',
+        keywords: ['man'],
+    }, {
+        id: 18,
+        url: 'image/18.jpg',
+        keywords: ['man'],
+    }]
+}
