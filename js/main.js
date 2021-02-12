e45134d5f129fba4d02543da42fd0b93f1afb540
ReamLasry <ream794613@gmail.com>
2021-02-12 23:24:47 +0200

    RESPONSIVE MIDDLE UPDATE

--- js/main.js
+++ js/main.js
@@ -24,8 +24,83 @@ function init() {
     createCanvas();
 }
 
+// PAGES FUNCTIONS
+function onRenderGallery() {
+    let strHTML = gImages.map(image => {
+        return `<div class="img${image.id}"><img  class="${image.id}" src="image/${image.id}.jpg" alt="" onclick="onRenderPhoto(this)"></div>`;
+    }).join('');
+    document.querySelector('.image-content').innerHTML = strHTML;
+}
+
+function onRenderMemes() {
+    let savedMemes = loadFromStorage(MEMESGALLERYKEY);
+    let strHTML;
+
+    if (!savedMemes) savedMemes = memesForDisplay();
+    if (savedMemes.length === 0) strHTML = '<h1>No memes yet...</h1>';
+    else strHTML = savedMemes.map(meme => {
+        return ` 
+            <div class="img${meme.id}">
+            <img class="delete-meme-img" id="${meme.id}" title="Delete meme" onclick="onDeleteMeme(this)" src="ICONS/trash.png" alt="">
+            <img  class="${meme.id}" src="${meme.url}" alt=""></div>`;
+    }).join('');
 
 
+    document.querySelector('.saved-memes-content').innerHTML = strHTML;
+}
+
+function onShowPage(el) {
+    let elGalleryPage = document.querySelector('.gallery');
+    let elSavedMemesPage = document.querySelector('.saved-memes')
+    let elMemesPage = document.querySelector('.main-memes');
+    let elAboutPage = document.querySelector('.about');
+
+    if (el.innerText === 'gallery'.toUpperCase()) {
+        elAboutPage.hidden = true;
+        elMemesPage.hidden = true;
+        elSavedMemesPage.hidden = true;
+        elGalleryPage.hidden = false;
+        onDeleteTextLines();
+    } else if (el.innerText === 'memes'.toUpperCase()) {
+        onRenderMemes();
+
+        elGalleryPage.hidden = true;
+        elAboutPage.hidden = true;
+        elMemesPage.hidden = true;
+        elSavedMemesPage.hidden = false
+    } else {
+        elSavedMemesPage.hidden = true;
+        elMemesPage.hidden = true;
+        elGalleryPage.hidden = true;
+        elAboutPage.hidden = false;
+    };
+}
+
+function onDeleteMeme(el) {
+    let elIdNum = +el.id;
+    deleteMeme(elIdNum);
+    onRenderMemes();
+}
+
+function onRenderPhoto(el) {
+    let imageId = +el.className;
+    changeSelectedPohto(imageId);
+
+    gCurrImgUrl = gImages[imageId - 1].url;
+    drawImg(gCurrImgUrl);
+
+    let elGalleryPage = document.querySelector('.gallery');
+    let elSavedMemesPage = document.querySelector('.saved-memes')
+    let elMemesPage = document.querySelector('.main-memes');
+    let elAboutPage = document.querySelector('.about');
+
+    elSavedMemesPage.hidden = true
+    elGalleryPage.hidden = true;
+    elAboutPage.hidden = true;
+    elMemesPage.hidden = false;
+}
+
+// CANVAS
 function createCanvas() {
     gElCanvas = document.querySelector('.canvas-content');
     gCtx = gElCanvas.getContext('2d');
@@ -43,14 +118,6 @@ function renderLines() {
     });
 }
 
-function onUpdateMemeText() {
-    let textInput = document.getElementById('text-input');
-    let textInputVal = textInput.value;
-    updateMemeText(textInputVal);
-    renderCanvas();
-}
-
-// Function to load image on the canvas
 function drawImg(url) {
     const img = new Image();
     img.src = url;
@@ -60,7 +127,6 @@ function drawImg(url) {
     };
 }
 
-// Function to add text on the canvas
 function drawText(text = '', fontSize = 20, fontColor = 'white', strokeColor = 'black', align = 'center', font = "ariel", x = gElCanvas.width / 2, y = 20) {
     gCtx.strokeStyle = strokeColor;
     gCtx.fillStyle = fontColor;
@@ -70,7 +136,14 @@ function drawText(text = '', fontSize = 20, fontColor = 'white', strokeColor = '
     gCtx.strokeText(text, x, y);
 }
 
-//Function to add new text line on the canvas.
+// MEME FUNCTIONS
+function onUpdateMemeText() {
+    let textInput = document.getElementById('text-input');
+    let textInputVal = textInput.value;
+    updateMemeText(textInputVal);
+    renderCanvas();
+}
+
 function onAddTextLine() {
     let textInput = document.getElementById('text-input');
     textInput.value = '';
@@ -80,14 +153,6 @@ function onAddTextLine() {
         gFirstLineAdd = !gFirstLineAdd;
     } else addTextLine(gElCanvas.height / 2);
     renderCanvas();
-}
-
-
-function onRenderGallery() {
-    let strHTML = gImages.map(image => {
-        return `<div class="img${image.id}"><img  class="${image.id}" src="image/${image.id}.jpg" alt="" onclick="onRenderPhoto(this)"></div>`;
-    }).join('');
-    document.querySelector('.image-content').innerHTML = strHTML;
 }
 
 function onDeleteTextLines() {
@@ -133,10 +198,6 @@ function onStrokeColor() {
     renderCanvas();
 }
 
-function clearCanvas() {
-    gCtx.clearRect(0, 0, gElCanvas.width, gElCanvas.height);
-}
-
 function onlineMove(el) {
     let elText = el.id;
     switch (elText) {
@@ -158,82 +219,45 @@ function onSwitchLine() {
     textInput.value = currLineTxt;
 }
 
-function onShowPage(el) {
-    let elGalleryPage = document.querySelector('.gallery');
-    let elSavedMemesPage = document.querySelector('.saved-memes')
-    let elMemesPage = document.querySelector('.main-memes');
-    let elAboutPage = document.querySelector('.about');
-
-    if (el.innerText === 'gallery'.toUpperCase()) {
-        elAboutPage.hidden = true;
-        elMemesPage.hidden = true;
-        elSavedMemesPage.hidden = true;
-        elGalleryPage.hidden = false;
-        onDeleteTextLines();
-    } else if (el.innerText === 'memes'.toUpperCase()) {
-        onRenderMemes();
-
-        elGalleryPage.hidden = true;
-        elAboutPage.hidden = true;
-        elMemesPage.hidden = true;
-        elSavedMemesPage.hidden = false
-    } else {
-        elSavedMemesPage.hidden = true;
-        elMemesPage.hidden = true;
-        elGalleryPage.hidden = true;
-        elAboutPage.hidden = false;
-    };
-}
-
-function onRenderMemes() {
-    let savedMemes = loadFromStorage(MEMESGALLERYKEY);
-    let strHTML;
-
-    if (!savedMemes) savedMemes = memesForDisplay();
-    if (savedMemes.length === 0) strHTML = '<h1>No memes yet...</h1>';
-    else strHTML = savedMemes.map(meme => {
-        return ` 
-            <div class="img${meme.id}">
-            <img class="delete-meme-img" id="${meme.id}" title="Delete meme" onclick="onDeleteMeme(this)" src="ICONS/trash.png" alt="">
-            <img  class="${meme.id}" src="${meme.url}" alt=""></div>`;
-    }).join('');
-
-
-    document.querySelector('.saved-memes-content').innerHTML = strHTML;
-}
-
-function onDeleteMeme(el) {
-    let elIdNum = +el.id;
-    deleteMeme(elIdNum);
-    onRenderMemes();
-}
-
-function onRenderPhoto(el) {
-    let imageId = +el.className;
-    changeSelectedPohto(imageId);
-
-    gCurrImgUrl = gImages[imageId - 1].url;
-    drawImg(gCurrImgUrl);
-
-    let elGalleryPage = document.querySelector('.gallery');
-    let elSavedMemesPage = document.querySelector('.saved-memes')
-    let elMemesPage = document.querySelector('.main-memes');
-    let elAboutPage = document.querySelector('.about');
-
-    elSavedMemesPage.hidden = true
-    elGalleryPage.hidden = true;
-    elAboutPage.hidden = true;
-    elMemesPage.hidden = false;
-}
-
 function downloadImg(ev, elLink) {
     ev.stopPropagation();
 
     let imgContent = gElCanvas.toDataURL('image/jpeg');
     elLink.href = imgContent;
+    _notifyMessege('download');
 }
 
 function saveMeme() {
     let dataURL = gElCanvas.toDataURL();
+
     pushToMemesGallery(dataURL);
+    _notifyMessege('save');
+}
+
+function shareImg() {
+    _notifyMessege('share');
+}
+
+function _notifyMessege(messegeType) {
+    let elModal = document.querySelector('.messege-modal');
+
+    switch (messegeType) {
+        case 'save':
+            elModal.innerText = 'Your meme is now save in the MEMES gallery!';
+            break;
+
+        case 'download':
+            elModal.innerText = 'Your download will begin shortly...';
+            break;
+        case 'share':
+            elModal.innerText = 'Unknown error, please try again later.';
+    }
+
+    elModal.style.visibility = 'unset';
+    setTimeout(_closeMessege, 2000);
+}
+
+function _closeMessege() {
+    let elModal = document.querySelector('.messege-modal');
+    elModal.style.visibility = 'hidden';
 }
